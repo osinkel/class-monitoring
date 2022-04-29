8406629b884d56d4ce8981b67c0e6158e48bc654
osinkel <oleg.sinkel.2012@mail.ru>
2022-04-29 16:27:41 +0300

    removed add/update/delete actions for subject/group/profile

--- timetable/views.py
+++ timetable/views.py
@@ -3,9 +3,6 @@ from django.core.exceptions import PermissionDenied
 from django.http import Http404
 from django.shortcuts import render
 from django.views import generic
-
-from timetable.forms import AddSubjectNameForm, UpdateSubjectNameForm, AddGroupForm, UpdateGroupForm, AddTeacherForm, \
-    UpdateTeacherForm
 from timetable.logic import get_subjects_name_of_user, get_groups_user_faculty, get_teachers_user_university, \
     get_students_user_group, get_timetables_user_group, get_all_dates_of_week, \
     get_name_of_days, get_all_subjects_of_user_teacher, get_subject_of_user, get_subjects_by_timetable, \
@@ -43,35 +40,6 @@ class TeacherDetailView(LoginRequiredMixin, generic.DetailView):
         return context
 
 
-class TeacherCreateView(LoginRequiredMixin, UserPassesTestMixin, generic.CreateView):
-    template_name = 'teachers/teacher_create_form.html'
-    model = Profile
-    form_class = AddTeacherForm
-    success_url = '/timetable/teachers'
-
-    def test_func(self):
-        return self.request.user.is_superuser or self.request.user.is_staff
-
-
-class TeacherDeleteView(LoginRequiredMixin, UserPassesTestMixin, generic.DeleteView):
-    template_name = 'teachers/teacher_confirm_delete.html'
-    model = Profile
-    success_url = '/timetable/teachers'
-
-    def test_func(self):
-        return self.request.user.is_superuser or self.request.user.is_staff
-
-
-class TeacherUpdateView(LoginRequiredMixin, UserPassesTestMixin, generic.UpdateView):
-    template_name = 'teachers/teacher_update_form.html'
-    model = Profile
-    form_class = UpdateTeacherForm
-    success_url = '/timetable/teachers'
-
-    def test_func(self):
-        return self.request.user.is_superuser or self.request.user.is_staff
-
-
 class GroupListView(LoginRequiredMixin, generic.ListView):
     template_name = 'groups/group_list.html'
     context_object_name = 'group_list'
@@ -96,35 +64,6 @@ class GroupDetailView(LoginRequiredMixin, generic.DetailView):
         return context
 
 
-class GroupCreateView(LoginRequiredMixin, UserPassesTestMixin, generic.CreateView):
-    template_name = 'groups/group_create_form.html'
-    model = Group
-    form_class = AddGroupForm
-    success_url = '/timetable/groups'
-
-    def test_func(self):
-        return self.request.user.is_superuser or self.request.user.is_staff
-
-
-class GroupDeleteView(LoginRequiredMixin, UserPassesTestMixin, generic.DeleteView):
-    template_name = 'groups/group_confirm_delete.html'
-    model = Group
-    success_url = '/timetable/groups'
-
-    def test_func(self):
-        return self.request.user.is_superuser or self.request.user.is_staff
-
-
-class GroupUpdateView(LoginRequiredMixin, UserPassesTestMixin, generic.UpdateView):
-    template_name = 'groups/group_update_form.html'
-    model = Group
-    form_class = UpdateGroupForm
-    success_url = '/timetable/groups'
-
-    def test_func(self):
-        return self.request.user.is_superuser or self.request.user.is_staff
-
-
 class SubjectNameListView(LoginRequiredMixin, generic.ListView):
     template_name = 'subjects/subject_list.html'
     context_object_name = 'subject_list'
@@ -147,35 +86,6 @@ class SubjectNameDetailView(LoginRequiredMixin, generic.DetailView):
         context = super().get_context_data(**kwargs)
         context['subjects'] = get_subject_of_user(self.request.user, context['object'])
         return context
-
-
-class SubjectNameCreateView(LoginRequiredMixin, UserPassesTestMixin, generic.CreateView):
-    template_name = 'subjects/subject_create_form.html'
-    model = SubjectName
-    form_class = AddSubjectNameForm
-    success_url = '/timetable/subjects'
-
-    def test_func(self):
-        return self.request.user.is_superuser or self.request.user.is_staff
-
-
-class SubjectNameDeleteView(LoginRequiredMixin, UserPassesTestMixin, generic.DeleteView):
-    template_name = 'subjects/subject_confirm_delete.html'
-    model = SubjectName
-    success_url = '/timetable/subjects'
-
-    def test_func(self):
-        return self.request.user.is_superuser or self.request.user.is_staff
-
-
-class SubjectNameUpdateView(LoginRequiredMixin, UserPassesTestMixin, generic.UpdateView):
-    template_name = 'subjects/subject_update_form.html'
-    model = SubjectName
-    form_class = UpdateSubjectNameForm
-    success_url = '/timetable/subjects'
-
-    def test_func(self):
-        return self.request.user.is_superuser or self.request.user.is_staff
 
 
 class TimetableListView(LoginRequiredMixin, generic.ListView):
